881b39309f3ecfc457c764b8b28ce1020eb81017
GyunHee <ef3232@naver.com>
2025-08-14 14:21:50 +0900

    init: init springboot project

---
+++ src/main/java/com/oreumi/oreumi_backend/OreumiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oreumi.oreumi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreumiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreumiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oreumi/oreumi_backend/OreumiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreumi.oreumi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreumiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
