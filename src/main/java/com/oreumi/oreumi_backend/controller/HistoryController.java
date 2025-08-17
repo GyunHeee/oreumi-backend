44bbe98ab428adcdcd2cc78fa7389357796ee235
sinyeowon <ioohyou@knu.ac.kr>
2025-08-17 16:27:35 +0900

    feat: 마이페이지 컨트롤러 생성

--- src/main/java/com/oreumi/oreumi_backend/controller/HistoryController.java
+++ src/main/java/com/oreumi/oreumi_backend/controller/HistoryController.java
@@ -1,4 +1,23 @@
 package com.oreumi.oreumi_backend.controller;
 
+import com.oreumi.oreumi_backend.controller.dto.HistoryResponse;
+import com.oreumi.oreumi_backend.service.history.HistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/history")
+@RequiredArgsConstructor
 public class HistoryController {
+
+    private final HistoryService historyService;
+
+    /**
+     * 히스토리 데이터 조회
+     * @return 히스토리 응답 데이터
+     */
+    @GetMapping
+    public HistoryResponse getHistoryData() {
+        return historyService.getHistoryData();
+    }
 }
